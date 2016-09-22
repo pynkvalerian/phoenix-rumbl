46736df35b5686e5e06c18249023f0865c25cf4f
pynkvalerian <yenping.chew90@gmail.com>
2016-09-22 17:40:30 +0800

    youtube api in js

--- web/router.ex
+++ web/router.ex
@@ -20,6 +20,7 @@ defmodule Rumbl.Router do
     get "/", PageController, :index
     resources "/users", UserController, only: [:index, :show, :new, :create]
     resources "/sessions", SessionController, only: [:new, :create, :delete]
+    resources "/watch/:id", WatchController
   end
 
   scope "/manage", Rumbl do
