606de75368403b4bda511e99f5866d4ace042a91
pynkvalerian <yenping.chew90@gmail.com>
2016-09-22 18:30:08 +0800

    implement slug with custom type in Rumbl.Permalink

--- web/router.ex
+++ web/router.ex
@@ -20,7 +20,7 @@ defmodule Rumbl.Router do
     get "/", PageController, :index
     resources "/users", UserController, only: [:index, :show, :new, :create]
     resources "/sessions", SessionController, only: [:new, :create, :delete]
-    resources "/watch/:id", WatchController
+    get "/watch/:id", WatchController, :show
   end
 
   scope "/manage", Rumbl do
