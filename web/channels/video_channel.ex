7c14b1c6cd5d74252cc202d3b1fbd296cc9411b8
pynkvalerian <yenping.chew90@gmail.com>
2016-09-23 13:10:32 +0800

    annotation model, create done

--- web/channels/video_channel.ex
+++ web/channels/video_channel.ex
@@ -5,21 +5,37 @@ defmodule Rumbl.VideoChannel do
   # socket.assigns (typically a map)
 
   def join("videos:" <> video_id, _params, socket) do
-    :timer.send_interval(5_000, :ping)
-    {:ok, socket}
+    {:ok, assign(socket, :video_id, String.to_integer(video_id))}
+  end
+
+  # make sure all incoming events have current_user
+  def handle_in(event, params, socket) do
+    user = Repo.get(Rumbl.User, socket.assigns.user_id)
+    handle_in(event, params, user, socket)
   end
 
   # handle new_annotation events pushed from client
-  def handle_in("new_annotation", params, socket) do
-    # then broadcast this event to all clients on this topic
-    # 3 args: socket, event_name, payload
-    broadcast! socket, "new_annotation", %{
-      user: %{username: "anon"},
-      body: params["body"],
-      at: params["at"]
-    }
+  def handle_in("new_annotation", params, user, socket) do
+    changeset =
+      # build association to user and set up Annotation changeset
+      user
+      |> build_assoc(:annotations, video_id: socket.assigns.video_id)
+      |> Rumbl.Annotation.changeset(params)
 
-    {:reply, :ok, socket}
+      case Repo.insert(changeset) do
+        {:ok, annotation} ->
+          # then broadcast this event to all clients on this topic
+          # 3 args: socket, event_name, payload
+          broadcast! socket, "new_annotation", %{
+            id: annotation.id,
+            user: Rumbl.UserView.render("user.json", %{user: user}),
+            body: annotation.body,
+            at: annotation.at
+          }
+          {:reply, :ok, socket}
+        {:error, changeset} ->
+          {:reply, {:error, %{errors: changeset}}, socket}
+      end
   end
 
   # callback invoked whenever msg reaches the channel
